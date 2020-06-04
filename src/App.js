f57479708160b8ec14dee13b494e8577f002fe64
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-06-04 14:54:15 +0530

    amplify

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import Routes from './Routes';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">Scratch</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <LinkContainer to="/signup">
-                <NavItem>Signup</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <NavItem>Login</NavItem>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Routes />
-      </div>
-    </Router>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'ap-south-1',
+    BUCKET: 'nd-note-app-uploads',
+  },
+  apiGateway: {
+    REGION: 'ap-south-1',
+    URL: 'https://lilwusxgtk.execute-api.ap-south-1.amazonaws.com/dev',
+  },
+  cognito: {
+    REGION: 'ap-south-1',
+    USER_POOL_ID: 'ap-south-1_tKDgJknwU',
+    APP_CLIENT_ID: '4qqbvnrprrp8hogj0j68n3vd1s',
+    IDENTITY_POOL_ID: 'ap-south-1:215fd56f-7249-48d7-a071-e62089506bc2',
+  },
+};
